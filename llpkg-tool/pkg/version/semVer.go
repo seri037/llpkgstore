16a4e41109a43a5f049141913a9260b0624e3ef2
Heuluck Lu <sonicth@foxmail.com>
2025-02-12 16:18:20 +0800

    feat: better version conversion format

--- llpkg-tool/pkg/version/semVer.go
+++ llpkg-tool/pkg/version/semVer.go
@@ -6,18 +6,18 @@ import (
 	"regexp"
 	"strings"
 
-	sem "github.com/Masterminds/semver/v3"
+	"github.com/Masterminds/semver/v3"
 )
 
 // Convert any C version into SemVer
-func ToSemVer(ver string) (*sem.Version, error) {
-	//At least two parts, "2","v1","20230607" are not allowed
+func ToSemVer(ver string) (*semver.Version, error) {
+	//At least two parts, "2","v1","20230607" would not be considered as SemVer
 	twoPartVer := regexp.MustCompile(`^v?(0|[1-9]\d*)(?:\.(0|[1-9]\d*))(?:\.(0|[1-9]\d*))?(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
 	if !twoPartVer.MatchString(ver) {
 		return insertVer(ver)
 	}
 	//Convert some “looks like SemVer” version numbers (v1.2.0, 1.3, etc) into SemVer
-	v, err := sem.NewVersion(ver)
+	v, err := semver.NewVersion(ver)
 	if err != nil {
 		return insertVer(ver)
 	}
@@ -25,17 +25,10 @@ func ToSemVer(ver string) (*sem.Version, error) {
 }
 
 // Add orginal version that is not SemVer to the pre-release part of "1.0.0"
-func insertVer(ver string) (*sem.Version, error) {
-	metaVersion := regexp.MustCompile(`^.+(\+).+$`)
-	var newVer string
-	//Before add to the pre-release part, add "-llgo" suffix before "+" (if exists), and replace "." with "-"
-	if metaVersion.MatchString(ver) {
-		newVer = strings.Replace(ver, "+", "-llgo+", 1)
-		newVer = fmt.Sprintf("1.0.0-%s", strings.ReplaceAll(newVer, ".", "-"))
-	} else {
-		newVer = fmt.Sprintf("1.0.0-%s-llgo", strings.ReplaceAll(ver, ".", "-"))
-	}
-	version, err := sem.StrictNewVersion(newVer)
+func insertVer(ver string) (*semver.Version, error) {
+	//Before add to the pre-release part, replace "." with "-"
+	newVer := fmt.Sprintf("0.0.0-0-%s", strings.ReplaceAll(ver, ".", "-"))
+	version, err := semver.StrictNewVersion(newVer)
 	if err != nil {
 		return nil, errors.New("Fail to convert " + ver)
 	} else {
