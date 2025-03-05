d349f96454ec630188c865a900e1ad0f338efaa5
Chengkun Chen <serix2004@outlook.com>
2025-03-05 17:07:43 +0800

    refactor: move upstream validation to the proper place

--- tools/pkg/config/config.go
+++ tools/pkg/config/config.go
@@ -1,11 +1,6 @@
 package config
 
-import (
-	"fmt"
-
-	"github.com/goplus/llpkg/tools/pkg/upstream"
-	"github.com/goplus/llpkg/tools/pkg/upstream/installer/conan"
-)
+var ValidInstallers = []string{"conan"}
 
 type LLpkgConfig struct {
 	UpstreamConfig UpstreamConfig `json:"upstream"`
@@ -24,21 +19,4 @@ type InstallerConfig struct {
 type PackageConfig struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
-}
-
-func NewUpstreamFromConfig(config UpstreamConfig) (upstream.Upstream, error) {
-	var installer upstream.Installer
-	switch config.InstallerConfig.Name {
-	case "conan":
-		installer = conan.NewConanInstaller(config.InstallerConfig.Config)
-	default:
-		return nil, fmt.Errorf("invalid configuration: upstream.installer.name %s is not supported", config.InstallerConfig.Name)
-	}
-
-	var up upstream.Upstream = upstream.NewDefaultUpstream(installer, upstream.Package{
-		Name:    config.PackageConfig.Name,
-		Version: config.PackageConfig.Version,
-	})
-
-	return up, nil
 }
--- tools/pkg/config/validate.go
+++ tools/pkg/config/validate.go
@@ -3,39 +3,27 @@ package config
 import (
 	"fmt"
 	"slices"
-
-	"github.com/goplus/llpkg/tools/pkg/upstream"
 )
 
 func ValidateLLpkgConfig(config LLpkgConfig) error {
-	return validateConfigUpstream(config)
+	return validateUpstreamConfig(config.UpstreamConfig)
 }
 
-func validateConfigUpstream(config LLpkgConfig) error {
+func validateUpstreamConfig(config UpstreamConfig) error {
 	// 1. check if upstream installer is valid
-	if config.UpstreamConfig.InstallerConfig.Name == "" {
+	if config.InstallerConfig.Name == "" {
 		return fmt.Errorf("invalid configuration: upstream.installer.name is required")
 	}
-	if !slices.Contains(upstream.ValidInstallers, config.UpstreamConfig.InstallerConfig.Name) {
-		return fmt.Errorf("invalid configuration: upstream.installer.name %s is not supported", config.UpstreamConfig.InstallerConfig.Name)
+	if !slices.Contains(ValidInstallers, config.InstallerConfig.Name) {
+		return fmt.Errorf("invalid configuration: upstream.installer.name %s is not supported", config.InstallerConfig.Name)
 	}
 
 	// 2. check if package is valid
-	if config.UpstreamConfig.PackageConfig.Name == "" {
+	if config.PackageConfig.Name == "" {
 		return fmt.Errorf("invalid configuration: upstream.package.name is required")
 	}
-	if config.UpstreamConfig.PackageConfig.Version == "" {
+	if config.PackageConfig.Version == "" {
 		return fmt.Errorf("invalid configuration: upstream.package.version is required")
-	}
-
-	// 3. build upstream, and check if package exists
-	up, err := NewUpstreamFromConfig(config.UpstreamConfig)
-	if err != nil {
-		return fmt.Errorf("failed to build upstream: %w", err)
-	}
-	_, err = up.Installer().Search(up.Package())
-	if err != nil {
-		return fmt.Errorf("failed to search for package %s: %w", up.Package().Name, err)
 	}
 
 	return nil
