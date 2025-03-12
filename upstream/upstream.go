3e30f8035b821b1d48437fef44a47433cf0ab192
Chengkun Chen <serix2004@outlook.com>
2025-03-12 15:42:42 +0800

    feat(upstream): export `Installer` and `Pkg` fields of struct `Upstream`

--- upstream/upstream.go
+++ upstream/upstream.go
@@ -1,26 +1,11 @@
 package upstream
 
 type Upstream struct {
-	installer Installer
-	pkg       Package
+	Installer Installer
+	Pkg       Package
 }
 
 type Package struct {
 	Name    string
 	Version string
-}
-
-func NewUpstream(installer Installer, pkg Package) *Upstream {
-	return &Upstream{
-		installer: installer,
-		pkg:       pkg,
-	}
-}
-
-func (u *Upstream) Installer() Installer {
-	return u.installer
-}
-
-func (u *Upstream) Package() Package {
-	return u.pkg
 }
